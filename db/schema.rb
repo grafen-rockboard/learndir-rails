5c5c88ef7defdd113ce4ac41c22b7c529f8ecbc5
Grafen <yanodon@gmail.com>
2021-06-30 12:12:47 +0900

    Rails: learn make new email validation on books.

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_23_154715) do
+ActiveRecord::Schema.define(version: 2021_06_30_025344) do
 
   create_table "books", force: :cascade do |t|
     t.string "name"
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "email"
   end
 
   create_table "shops", force: :cascade do |t|
